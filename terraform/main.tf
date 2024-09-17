215b0183147140f0ba1e0f41078711cbb2285658
Marcelo Gomes <marcelo.gomes@rededor.com.br>
2024-09-17 12:22:21 -0300

    removendo profile do provider do terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "career_path" {
-  bucket = "career-path-terraform-studies"
+  bucket = "career-path-terraform-studies"  # Certifique-se de que o nome do bucket é único
 
   tags = {
     Name        = "My S3 Bucket"
