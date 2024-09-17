a21e76d26061e1fc52a0e988b171f90c0df4e321
Marcelo Gomes <marcelo.gomes@rededor.com.br>
2024-09-17 13:27:48 -0300

    new pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-career-path"  # O bucket onde o estado será armazenado
+    key    = "terraform/career_path_terraform.tfstate"  # Caminho dentro do bucket
+    region = "us-east-1"
+    encrypt = true  # Criptografar o arquivo de estado no S3
+  }
+}
+
 resource "aws_s3_bucket" "career_path" {
   bucket = "career-path-terraform-studies"  # Certifique-se de que o nome do bucket é único
 
