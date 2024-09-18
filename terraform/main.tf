12d888f356e85e9d0dfc171f7918d16f59c56b71
Marcelo Gomes <marcelo.gomes@rededor.com.br>
2024-09-18 16:14:18 -0300

    Correção Glue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
-# Criação do Bucket S3
+# Criação do Bucket S3 para armazenar os dados Parquet
 resource "aws_s3_bucket" "career_path" {
-  bucket = "career-path-terraform-studies"  # Nome do bucket S3
-
+  bucket = "career-path-terraform-studies"
   force_destroy = true
 
   tags = {
@@ -10,10 +9,9 @@ resource "aws_s3_bucket" "career_path" {
   }
 }
 
-# Criação do Bucket S3 para Resultados do Athenaa
+# Criação do Bucket S3 para Resultados do Athena
 resource "aws_s3_bucket" "athena_results" {
   bucket = "athena-query-results-career-path"
-
   force_destroy = true
 
   tags = {
@@ -57,33 +55,28 @@ resource "aws_glue_catalog_database" "career_path_db" {
   name = "career_path_db"
 }
 
-# Criação da Tabela no Glue com Dados em formato Parquet
-resource "aws_glue_catalog_table" "career_path_table" {
-  name          = "career_path_table"
+# Criação do Glue Crawler para detectar automaticamente o esquema e criar a tabela
+resource "aws_glue_crawler" "career_path_crawler" {
+  name          = "career-path-crawler"
+  role          = aws_iam_role.glue_role.arn  # O Role do Glue
   database_name = aws_glue_catalog_database.career_path_db.name
-  table_type    = "EXTERNAL_TABLE"
 
-  storage_descriptor {
-    location      = "s3://${aws_s3_bucket.career_path.bucket}/data/"  # Diretório no S3 que contém arquivos Parquet
-    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
-    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
-    compressed    = false  # Ajustar para 'true' se o arquivo Parquet estiver comprimido
-
-    # Definição das colunas do Parquet
-    columns {
-      name = "title"
-      type = "string"
-    }
-
-    columns {
-      name = "experience"
-      type = "string"
-    }
-
-    ser_de_info {
-      name = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
-    }
+  s3_target {
+    path = "s3://${aws_s3_bucket.career_path.bucket}/data/"  # Caminho S3 onde os arquivos Parquet estão
   }
+
+  configuration = jsonencode({
+    "Version" : 1.0,
+    "Grouping": {
+      "TableGroupingPolicy": "CombineCompatibleSchemas"
+    }
+  })
+
+  schedule = "cron(0 12 * * ? *)"  # Opcional: rodar diariamente ao meio-dia
+
+  depends_on = [
+    aws_iam_role_policy_attachment.glue_s3_policy_attach
+  ]
 }
 
 # Criação do IAM Role para o Glue
@@ -184,4 +177,40 @@ resource "aws_iam_policy" "athena_s3_policy" {
 resource "aws_iam_role_policy_attachment" "athena_s3_policy_attach" {
   role       = aws_iam_role.athena_role.name
   policy_arn = aws_iam_policy.athena_s3_policy.arn
+}
+
+# Política para permitir que o Glue Crawler acesse e atualize o Glue Data Catalog
+resource "aws_iam_policy" "glue_crawler_policy" {
+  name = "GlueCrawlerPolicy"
+
+  policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "glue:GetTable",
+          "glue:CreateTable",
+          "glue:UpdateTable",
+          "glue:DeleteTable",
+          "glue:GetDatabase",
+          "glue:CreateDatabase",
+          "glue:UpdateDatabase"
+        ],
+        "Resource": [
+          "arn:aws:glue:*:*:catalog",
+          "arn:aws:glue:*:*:database/${aws_glue_catalog_database.career_path_db.name}",
+          "arn:aws:glue:*:*:table/${aws_glue_catalog_database.career_path_db.name}/*"
+        ]
+      }
+    ]
+  }
+  EOF
+}
+
+# Anexando a política ao IAM Role do Glue
+resource "aws_iam_role_policy_attachment" "glue_crawler_policy_attach" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = aws_iam_policy.glue_crawler_policy.arn
 }
