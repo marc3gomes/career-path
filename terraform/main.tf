59d35f795f30c6e24821ec581b0a0c74cd298aca
Marcelo Gomes <marcelo.gomes@rededor.com.br>
2024-09-18 17:17:38 -0300

    Correção Glue

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,7 @@
-# Criação do Bucket S3 para armazenar os dados Parquet
+# Criação do Bucket S3
 resource "aws_s3_bucket" "career_path" {
-  bucket = "career-path-terraform-studies"
+  bucket = "career-path-terraform-studies"  # Nome do bucket S3
+
   force_destroy = true
 
   tags = {
@@ -12,6 +13,7 @@ resource "aws_s3_bucket" "career_path" {
 # Criação do Bucket S3 para Resultados do Athena
 resource "aws_s3_bucket" "athena_results" {
   bucket = "athena-query-results-career-path"
+
   force_destroy = true
 
   tags = {
@@ -20,178 +22,36 @@ resource "aws_s3_bucket" "athena_results" {
   }
 }
 
-# Política do bucket S3 para permitir acesso ao Glue e Athena
-resource "aws_s3_bucket_policy" "career_path_policy" {
-  bucket = aws_s3_bucket.career_path.bucket
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": [
-            "athena.amazonaws.com",
-            "glue.amazonaws.com"
-          ]
-        },
-        "Action": [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        "Resource": [
-          "arn:aws:s3:::${aws_s3_bucket.career_path.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.career_path.bucket}/*"
-        ]
-      }
-    ]
-  }
-  EOF
-}
-
 # Criação do Glue Database
 resource "aws_glue_catalog_database" "career_path_db" {
   name = "career_path_db"
 }
 
-# Criação do Glue Crawler para detectar automaticamente o esquema e criar a tabela
-resource "aws_glue_crawler" "career_path_crawler" {
-  name          = "career-path-crawler"
-  role          = aws_iam_role.glue_role.arn
+# Criação da Tabela no Glue com Dados Não-Aninhados
+resource "aws_glue_catalog_table" "career_path_table" {
+  name          = "career_path_table"
   database_name = aws_glue_catalog_database.career_path_db.name
+  table_type    = "EXTERNAL_TABLE"
 
-  s3_target {
-    path = "s3://${aws_s3_bucket.career_path.bucket}/data/"
-  }
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.career_path.bucket}/data/"  # Diretório no S3 que contém arquivos Parquet
+    input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
+    output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
+    compressed    = false  # Ajustar para 'true' se o arquivo Parquet estiver comprimido
 
-  configuration = jsonencode({
-    "Version" : 1.0,
-    "Grouping": {
-      "TableGroupingPolicy": "CombineCompatibleSchemas"
+    # Definição das colunas do Parquet
+    columns {
+      name = "title"
+      type = "string"
     }
-  })
 
-  schedule = "cron(0 12 * * ? *)"  # Opcional: rodar diariamente ao meio-dia
-}
+    columns {
+      name = "experience"
+      type = "string"
+    }
 
-# Criação do IAM Role para o Glue
-resource "aws_iam_role" "glue_role" {
-  name = "glue-service-role"
-
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "glue.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
+    ser_de_info {
+      name = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
+    }
   }
-  EOF
-}
-
-# Política inline do IAM Role para o Glue acessar o S3
-resource "aws_iam_role_policy" "glue_s3_policy" {
-  name = "GlueS3AccessPolicy"
-  role = aws_iam_role.glue_role.id
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        "Resource": [
-          "arn:aws:s3:::${aws_s3_bucket.career_path.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.career_path.bucket}/*"
-        ]
-      }
-    ]
-  }
-  EOF
-}
-
-# Criação do IAM Role para o Athena
-resource "aws_iam_role" "athena_role" {
-  name = "athena-service-role"
-
-  assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "athena.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
-    ]
-  }
-  EOF
-}
-
-# Política inline do IAM Role para o Athena acessar o S3
-resource "aws_iam_role_policy" "athena_s3_policy" {
-  name = "AthenaS3AccessPolicy"
-  role = aws_iam_role.athena_role.id
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        "Resource": [
-          "arn:aws:s3:::${aws_s3_bucket.career_path.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.career_path.bucket}/*"
-        ]
-      }
-    ]
-  }
-  EOF
-}
-
-# Política inline para permitir que o Glue Crawler acesse e atualize o Glue Data Catalog
-resource "aws_iam_role_policy" "glue_crawler_policy" {
-  name = "GlueCrawlerPolicy"
-  role = aws_iam_role.glue_role.id
-
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "glue:GetTable",
-          "glue:CreateTable",
-          "glue:UpdateTable",
-          "glue:DeleteTable",
-          "glue:GetDatabase",
-          "glue:CreateDatabase",
-          "glue:UpdateDatabase"
-        ],
-        "Resource": [
-          "arn:aws:glue:*:*:catalog",
-          "arn:aws:glue:*:*:database/${aws_glue_catalog_database.career_path_db.name}",
-          "arn:aws:glue:*:*:table/${aws_glue_catalog_database.career_path_db.name}/*"
-        ]
-      }
-    ]
-  }
-  EOF
 }
