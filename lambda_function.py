e6b05062e9ea1746dc5fa05977194c711b839c61
Marcelo Gomes <marcelo.gomes@rededor.com.br>
2024-09-20 11:24:40 -0300

    adding api gateway for lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,64 +1,55 @@
+import json
 import boto3
 import os
 import time
-import json
 
 athena = boto3.client('athena')
 
 def handler(event, context):
-    # Logando o evento recebido para garantir que a função está recebendo os dados corretamente
-    print("Evento recebido:", json.dumps(event))
+    # Debug para ver o corpo da requisição recebido
+    print("Evento recebido:", event)
 
     # Nome do banco de dados Athena e o local para salvar os resultados
     database = os.environ['ATHENA_DATABASE']
     output_location = os.environ['ATHENA_OUTPUT']
 
-    # Verificar se a chave 'query' está presente no evento
-    query = event.get('query')
-    if not query:
-        print("Consulta SQL não encontrada no corpo da requisição.")
+    # Query SQL recebida do evento (adicionando tratamento extra para o corpo)
+    try:
+        body = json.loads(event["body"])
+        query = body['query']
+    except KeyError:
         return {
             'statusCode': 400,
             'body': 'A consulta SQL ("query") não foi fornecida no corpo da requisição.'
         }
 
-    print(f"Executando consulta: {query}")
-
     # Executando a consulta no Athena
-    try:
-        response = athena.start_query_execution(
-            QueryString=query,
-            QueryExecutionContext={'Database': database},
-            ResultConfiguration={'OutputLocation': output_location}
-        )
+    response = athena.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={'Database': database},
+        ResultConfiguration={'OutputLocation': output_location}
+    )
 
-        # ID da execução da consulta
-        query_execution_id = response['QueryExecutionId']
+    # ID da execução da consulta
+    query_execution_id = response['QueryExecutionId']
 
-        # Aguardar a execução ser concluída
-        while True:
-            status = athena.get_query_execution(QueryExecutionId=query_execution_id)['QueryExecution']['Status']['State']
-            if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
-                break
-            time.sleep(1)
+    # Aguardar a execução ser concluída
+    while True:
+        status = athena.get_query_execution(QueryExecutionId=query_execution_id)['QueryExecution']['Status']['State']
+        if status in ['SUCCEEDED', 'FAILED', 'CANCELLED']:
+            break
+        time.sleep(1)
 
-        # Verificar se a consulta foi bem-sucedida
-        if status == 'SUCCEEDED':
-            # Recuperar os resultados da consulta
-            result = athena.get_query_results(QueryExecutionId=query_execution_id)
-            return {
-                'statusCode': 200,
-                'body': result
-            }
-        else:
-            return {
-                'statusCode': 500,
-                'body': f"Query {status}"
-            }
-
-    except Exception as e:
-        print(f"Erro ao executar a consulta: {str(e)}")
+    # Verificar se a consulta foi bem-sucedida
+    if status == 'SUCCEEDED':
+        # Recuperar os resultados da consulta
+        result = athena.get_query_results(QueryExecutionId=query_execution_id)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(result)
+        }
+    else:
         return {
             'statusCode': 500,
-            'body': f"Erro ao executar a consulta: {str(e)}"
+            'body': f"Query {status}"
         }
