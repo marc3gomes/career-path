0061aff9854a68371001f9f2f89f16b051adbee8
Marcelo Gomes <marcelo.gomes@rededor.com.br>
2024-09-20 15:47:48 -0300

    adding api gateway for lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,8 +36,7 @@ resource "aws_s3_bucket_policy" "athena_results_policy" {
         "Action": [
           "s3:GetObject",
           "s3:PutObject",
-          "s3:ListBucket",
-          "s3:GetBucketLocation"
+          "s3:ListBucket"
         ],
         "Resource": [
           "arn:aws:s3:::${aws_s3_bucket.athena_results.bucket}",
@@ -63,7 +62,7 @@ resource "aws_iam_role_policy" "lambda_athena_results_policy" {
         "Action": [
           "s3:GetObject",
           "s3:PutObject",
-          "s3:ListBucket",
+          "s3:ListBucket"
           "s3:GetBucketLocation"
         ],
         "Resource": [
